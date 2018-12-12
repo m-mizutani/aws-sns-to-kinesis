699f3779ca7cd60ca08f2e00274ed6686956ecf8
Masayoshi Mizutani <mizutani@cookpad.com>
2018-12-13 00:22:10 +0900

    Split main logic and function

--- main.go
+++ main.go
@@ -2,88 +2,14 @@ package main
 
 import (
 	"context"
-	"crypto/sha256"
-	"fmt"
 	"os"
-	"strings"
 
 	log "github.com/sirupsen/logrus"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/kinesis"
-	// "github.com/aws/aws-lambda-go/lambdacontext"
+	sns2kinesis "github.com/m-mizutani/aws-sns-to-kinesis/lib"
 )
-
-// Argument is for Handler
-type Argument struct {
-	Sns       events.SNSEvent
-	StreamArn string
-}
-
-type ResultLog struct {
-	Message string `json:"message"`
-	Done    bool   `json:"done"`
-	Error   string `json:"error"`
-}
-type Result struct {
-	Logs []*ResultLog `json:"logs"`
-}
-
-func makeKinesisClient(arn string) (*kinesis.Kinesis, string) {
-	arr := strings.Split(arn, ":")
-	if len(arr) != 6 {
-		log.WithField("invalid_arn", arn).Error("Invali ARN format")
-	}
-	nameParts := strings.Split(arr[5], "/")
-	if len(nameParts) != 2 || nameParts[0] != "stream" {
-		log.WithField("invalid_arn", arn).Error("Invali ARN format (must be 'stream/*'")
-	}
-
-	ssn := session.Must(session.NewSession(&aws.Config{
-		Region: aws.String(arr[3]),
-	}))
-	kinesisClient := kinesis.New(ssn)
-
-	return kinesisClient, nameParts[1]
-}
-
-// Handler is a main function, should be called from test
-func Handler(args Argument) (Result, error) {
-	log.WithField("args", args).Info("Start")
-	var result Result
-
-	kinesisClient, streamName := makeKinesisClient(args.StreamArn)
-	input := kinesis.PutRecordsInput{
-		StreamName: aws.String(streamName),
-	}
-
-	for _, record := range args.Sns.Records {
-		data := record.SNS.Message
-
-		reslog := ResultLog{Message: data}
-		result.Logs = append(result.Logs, &reslog)
-
-		entry := kinesis.PutRecordsRequestEntry{
-			Data:         []byte(data),
-			PartitionKey: aws.String(fmt.Sprintf("%x", sha256.Sum256([]byte(data)))),
-		}
-
-		input.Records = append(input.Records, &entry)
-		reslog.Done = true
-	}
-
-	_, err := kinesisClient.PutRecords(&input)
-
-	if err != nil {
-		log.WithField("input", input).Error("Kinesis PutRecords error")
-	}
-
-	log.WithField("result", result).Info("Exit normally")
-	return result, nil
-}
 
 func main() {
 	log.SetFormatter(&log.JSONFormatter{})
@@ -101,8 +27,8 @@ func main() {
 		log.WithField("log_level", os.Getenv("LOG_LEVEL")).Info("no properly log level setting, set InfoLevel as default")
 	}
 
-	lambda.Start(func(ctx context.Context, ev events.SNSEvent) (Result, error) {
-		pusherEvent := Argument{ev, os.Getenv("DST_STREAM")}
-		return Handler(pusherEvent)
+	lambda.Start(func(ctx context.Context, ev events.SNSEvent) (sns2kinesis.Result, error) {
+		pusherEvent := sns2kinesis.Argument{ev, os.Getenv("DST_STREAM")}
+		return sns2kinesis.Handler(pusherEvent)
 	})
 }
